868c14cfd8bb30764920c6b4fc6705febe0c8aec
vjfernandez <victor.fernandezgil@educa.madrid.org>
2015-07-25 00:15:27 +0200

    otra vez heroku

---
+++ models/quiz.js
@@ -0,0 +1,10 @@
+module.exports=function(sequelize, DataTypes)
+{
+   return sequelize.define('Quiz',
+       {
+	       pregunta: DataTypes.STRING,
+		   respuesta: DataTypes.STRING
+	   }
+	);
+	
+}
