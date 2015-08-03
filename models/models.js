bb012f0edb12003aa4cab0d29e005f169949dd84
vjfernandez <victor.fernandezgil@educa.madrid.org>
2015-08-03 18:45:01 +0200

    Añadido dependencia pg-hstore para heroku

--- models/models.js
+++ models/models.js
@@ -17,7 +17,7 @@ var Sequelize=require('sequelize');
 
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd, 
-  { dialect:  protocol,
+  { dialect:  dialect,
     protocol: protocol,
     port:     port,
     host:     host,
