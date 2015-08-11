65f43e0a0f73b7e29292953144b72e617a6cbf27
vjfernandez <victor.fernandezgil@educa.madrid.org>
2015-08-11 19:18:11 +0200

    Con borrado. modulo 8. tema 4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -108,3 +108,9 @@ exports.update = function(req, res) {
     }); //then
 };
 
+exports.destroy=function(req,res) {
+    req.quiz.destroy().then(function() {
+        res.redirect('/quizes');
+    }).catch(function(error){next(error)});
+}
+
--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,8 @@ router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 
+
+//borrar
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
 module.exports = router;
